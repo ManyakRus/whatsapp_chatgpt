5d3ad7347876cfb8156ef2ad3480a045cfa97a3f
Nikitin Aleksandr <nikitinav@atomsbt.ru>
2023-03-03 11:47:39 +0300

    сделал messW.String()

--- internal/v0/app/whatsapp/whatsapp.go
+++ internal/v0/app/whatsapp/whatsapp.go
@@ -23,7 +23,7 @@ func EventHandler(evt interface{}) {
 		mess := evt.(*events.Message)
 		messW := whatsapp_connect.FillMessageWhatsapp(mess)
 		//fmt.Println("Received a message from: ", messW.NameFrom, " phone: ", messW.PhoneFrom, "text: ", messW.Text)
-		log.Debug("new message:\n", messW.String())
+		log.Debug("new message:\n", messW.String(), "\n")
 		ReceiveMessage(messW)
 		//fmt.Println("Received a message: ", mess.Message, " from: ", v.Message.GetContactMessage(), "text: ", v.Info.MediaType)
 	default:
